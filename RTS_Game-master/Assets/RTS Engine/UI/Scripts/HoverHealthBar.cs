6a7db62d6ddbae1d206f0d3b139d55ba903c2164
任森烨 <318997802@qq.com>
2022-03-15 17:10:38 +0800

    2022/3/15 17:00

---
+++ RTS_Game-master/Assets/RTS Engine/UI/Scripts/HoverHealthBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/* Hover Health Bar script created by Oussama Bouanani, SoumiDelRio.
+ * This script is part of the Unity RTS Engine */
+
+namespace RTSEngine
+{
+    public class HoverHealthBar : MonoBehaviour
+    {
+        //make sure the hover health bar is always looking at the camera
+        //确保悬停健康栏始终注视着摄像头
+        Transform CamTransform;
+
+        void Start ()
+        {
+            CamTransform = GameManager.Instance.CamMov.transform;
+        }
+
+        void Update()
+        {
+            //move the canvas in order to face the camera and look at it
+            //移动画布以面对相机并观察它 
+            transform.LookAt(transform.position + CamTransform.rotation * Vector3.forward,
+                CamTransform.rotation * Vector3.up);
+        }
+    }
+}
